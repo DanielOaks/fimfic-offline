52dc78b161fe91eda27081bccf02fda41ba1e8dc
Daniel Oakley <danneh@danneh.net>
2012-08-18 03:23:50 +1000

    Initial Commit

---
+++ cl/js/fim.js
@@ -0,0 +1,192 @@
+/* FIMFiction Offline Reader
+**  Basically, downloads every story in the user's Read Later list
+**  to localStorage. The fimfic class below basically creates a nice
+**  little class to abstract all that out.
+**
+**  Now, it doesn't work with normal browsers yet, since we're not
+**  allowed to do this via the same origin policy. Pesky security!
+*/
+
+
+var fimfic = {
+	isOnline: false,
+	isLoggedIn: false,
+	stories: [],
+
+	// init fimfic connection, see if online and/or logged in
+	initConnection: function (callback) {
+		this.testLoggedIn(function () {
+			callback.call();
+		});
+	},
+
+	// test whether the user is logged into FIMFiction, set flag isLoggedIn
+	testLoggedIn: function (callback) {
+		var request = $.ajax({
+			url: "http://fimfiction.net"
+		});
+
+
+		request.done(function(html) {
+			fimfic.isOnline = true;
+
+			var parsedhtml = $(html);
+			fimfic.checkLoggedIn(parsedhtml);
+
+			callback.call();
+		});
+
+
+		request.fail(function(jqXHR, textStatus) {
+			fimfic.isOnline = false;
+			fimfic.isLoggedIn = false;
+
+			callback.call();
+		});
+	}, 
+
+	// init fimfic database
+	initDatabase: function (callback) {
+		fimfic.db = $.indexedDB("fimfic_offline", {
+			"version": 1,
+			"upgrade": function(transaction) {
+				// initialise database, if not already done so
+				transaction.createObjectStore("stories");
+			}
+		});
+	},
+
+	// update list of stories
+	updateStories: function () {
+		console.log("Returning stories in user's Read Later list");
+
+		fimfic.listStories(function () {
+			// do stuff in here, etc
+		});
+	},
+
+	// return list of stories in read later list, from site
+	listStories: function (callback) {
+		var request = $.ajax({
+			url: "http://www.fimfiction.net/index.php?view=category&read_it_later=1&compact_view=1"
+		});
+
+
+		fimfic.listedStories = [] // Will either get populated by .done, or be left blank
+
+
+		request.done(function(html) {
+			fimfic.isOnline = true;
+
+			var parsedhtml = $(html);
+			fimfic.checkLoggedIn(parsedhtml);
+
+			$('#content').append($('<div id="stories"></div>'));
+
+			// the following code loops over each row of story table, and manually extracts story data
+			$.each($(parsedhtml).find('#archive_table').children('tbody').children(), function() {
+				var id = $(this).find('u a').attr('href').split('/')[2];
+				var title = $(this).find('u a').text();
+				var description = $(this).find('.description').text();
+				var author = $($($(this).children()[4]).children()[0]).text();
+
+				//console.log('Adding Story: ' + title)
+
+				fimfic.listedStories.push({
+					'id': id,
+					'title': title,
+					'description': description,
+					'author': author
+				});
+
+				story = $('<div class="story"></div>');
+
+
+				storyheader = $('<div class="head"></div>');
+				story.append(storyheader);
+
+				storyheader.append($('<h2></h2>').text(title));
+				storyheader.append($('<span>&nbsp;&nbsp;by </span>'));
+				storyheader.append($('<span class="author"></span>').text(author));
+
+
+				storybody = $('<div class="body"></div>');
+				story.append(storybody);
+
+				storybody.append($('<span class="description"></span>').text(description));
+
+				$('#stories').append(story);
+			});
+
+			callback.call();
+		});
+
+
+		request.fail(function(jqXHR, textStatus) {
+			callback.call();
+		});
+	},
+
+	// set isLoggedIn variable, given html
+	//
+	// we do this every request, as isLoggedIn is
+	//  only used as an info variable, rather than
+	//  to actually keep us from programatically
+	//  making requests... Nicer to keep everything
+	//  totally dynamic, I think.
+	checkLoggedIn: function (parsedhtml) {
+		// see whether logged in
+		fimfic.isLoggedIn = true;
+
+		// find nav_bar
+		$.each(parsedhtml, function() {
+		if (this.className == 'nav_bar') {
+
+			// find light
+			$.each(this, function() {
+			if (this.className == 'light') {
+
+				// find right
+				$.each(this, function() {
+				if (this.className == 'right') {
+
+					// find login area
+					$.each(this, function() {
+						$.each(this, function() {
+							if (this.id == 'login_area') {
+								// login_area div only shown
+								//  when user has yet to login
+								fimfic.isLoggedIn = false;
+								return;
+							}
+						})
+
+					})
+				}})
+			}})
+		}});
+	}
+}
+
+
+$(document).ready(function() {
+	fimfic.initConnection(function() {
+
+		// Initialise database, if not done so already
+		fimfic.initDatabase();
+
+		// Check 'net connection
+		if (fimfic.isLoggedIn) {
+			$('#status').text("Checking Read Later list");
+		} else if (fimfic.isOnline) {
+			$('#status').text("You need to login to fimfiction to use this site");
+		} else {
+			$('#status').text("You need internet access to download stories (or cross-site ajax error)");
+			// or, you know, the cross-site xmlhttprequest hasn't been setup yet
+		}
+		$('#status').delay(5000).fadeOut(400);
+
+		fimfic.updateStories();
+
+	});
+});
