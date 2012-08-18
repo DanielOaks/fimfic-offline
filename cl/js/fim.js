9246cb898233bc9552b0f48c24c0a2962b4c3f5c
Daniel Oakley <danneh@danneh.net>
2012-08-18 18:07:55 +1000

    Removed spinner files, indentation to spaces, working on IndexedDB

--- cl/js/fim.js
+++ cl/js/fim.js
@@ -9,187 +9,198 @@
 
 
 var fimfic = {
-	isOnline: false,
-	isLoggedIn: false,
-	stories: [],
+    isOnline: false,
+    isLoggedIn: false,
+    stories: [],
 
-	// init fimfic connection, see if online and/or logged in
-	initConnection: function (callback) {
-		this.testLoggedIn(function () {
-			callback.call();
-		});
-	},
+    // init fimfic connection, see if online and/or logged in
+    initConnection: function (callback) {
+        this.testLoggedIn(function () {
+            callback.call();
+        });
+    },
 
-	// test whether the user is logged into FIMFiction, set flag isLoggedIn
-	testLoggedIn: function (callback) {
-		var request = $.ajax({
-			url: "http://fimfiction.net"
-		});
-
-
-		request.done(function (html) {
-			fimfic.isOnline = true;
-
-			var parsedhtml = $(html);
-			fimfic.checkLoggedIn(parsedhtml);
-
-			callback.call();
-		});
+    // test whether the user is logged into FIMFiction, set flag isLoggedIn
+    testLoggedIn: function (callback) {
+        var request = $.ajax({
+            url: "http://fimfiction.net"
+        });
 
 
-		request.fail(function (jqXHR, textStatus) {
-			fimfic.isOnline = false;
-			fimfic.isLoggedIn = false;
+        request.done(function (html) {
+            fimfic.isOnline = true;
 
-			callback.call();
-		});
-	}, 
+            var parsedhtml = $(html);
+            fimfic.checkLoggedIn(parsedhtml);
 
-	// init fimfic database
-	initDatabase: function (callback) {
-		fimfic.db = $.indexedDB("fimfic_offline", {
-			"version": 1,
-			"upgrade": function(transaction) {
-				// initialise database, if not already done so
-				transaction.createObjectStore("stories");
-			}
-		});
-	},
-
-	// update list of stories
-	updateStories: function (callback) {
-		console.log("Returning stories in user's Read Later list");
-
-		fimfic.listStories(function (code) {
-
-			// replace db stories with listed stories
-
-			callback.call(code);
-		});
-	},
-
-	// return list of stories in read later list, from site
-	listStories: function (callback) {
-		var request = $.ajax({
-			url: "http://www.fimfiction.net/index.php?view=category&read_it_later=1&compact_view=1"
-		});
+            callback.call();
+        });
 
 
-		fimfic.listedStories = [] // Will either get populated by .done, or be left blank
+        request.fail(function (jqXHR, textStatus) {
+            fimfic.isOnline = false;
+            fimfic.isLoggedIn = false;
+
+            callback.call();
+        });
+    }, 
+
+    // init fimfic database
+    initDatabase: function () {
+        fimfic.db = $.indexedDB("fimfic_offline", {
+            "version": 84,
+            "upgrade": function(transaction) {
+                // initialise database, if not already done so
+                console.log('Database Initialising')
+                transaction.createObjectStore("meta"); // stores app metadata, list of cached stories and such
+
+                transaction.createObjectStore("story_info"); // stores story info, metadata and such (think fimfiction API)
+                transaction.createObjectStore("story_html"); // stores the actual story html
+            }
+        });
+    },
+
+    // update list of stories
+    updateStories: function (callback) {
+        console.log("Returning stories in user's Read Later list");
+
+        fimfic.listStories(function (code) {
+
+            // replace db stories with listed stories
+
+            callback.call(code);
+        });
+    },
+
+    // return list of stories in read later list, from site
+    listStories: function (callback) {
+        var request = $.ajax({
+            url: "http://www.fimfiction.net/index.php?view=category&read_it_later=1&compact_view=1"
+        });
 
 
-		request.done(function (html) {
-			fimfic.isOnline = true;
-
-			var parsedhtml = $(html);
-			fimfic.checkLoggedIn(parsedhtml);
-
-			// the following code loops over each row of story table, and manually extracts story data
-			$.each($(parsedhtml).find('#archive_table').children('tbody').children(), function() {
-				var id = $(this).find('u a').attr('href').split('/')[2];
-				var title = $(this).find('u a').text();
-				var description = $(this).find('.description').text();
-				var author = $($($(this).children()[4]).children()[0]).text();
-
-				//console.log('Adding Story: ' + title)
-
-				fimfic.listedStories.push({
-					'id': id,
-					'title': title,
-					'description': description,
-					'author': author
-				});
-			});
-
-			fimfic.listStoriesStatus = 'success';
-			callback.call();
-		});
+        fimfic.listedStories = [] // Will either get populated by .done, or be left blank
 
 
-		request.fail(function(jqXHR, textStatus) {
-			fimfic.isOnline = false;
-			fimfic.isLoggedIn = false;
-			fimfic.listStoriesStatus = 'success';
-			callback.call();
-		});
-	},
+        request.done(function (html) {
+            fimfic.isOnline = true;
 
-	showListedStories: function () {
-		$('#content').append($('<div id="stories"></div>'));
+            var parsedhtml = $(html);
+            fimfic.checkLoggedIn(parsedhtml);
 
-		$.each(fimfic.listedStories, function() {
+            // the following code loops over each row of story table, and manually extracts story data
+            $.each($(parsedhtml).find('#archive_table').children('tbody').children(), function() {
+                var id = $(this).find('u a').attr('href').split('/')[2];
+                var title = $(this).find('u a').text();
+                var description = $(this).find('.description').text();
+                var author = $($($(this).children()[4]).children()[0]).text();
 
-			story = $('<div class="story"></div>');
+                //console.log('Adding Story: ' + title)
+
+                fimfic.listedStories.push({
+                    'id': id,
+                    'title': title,
+                    'description': description,
+                    'author': author
+                });
+            });
+
+            fimfic.listStoriesStatus = 'success';
+            callback.call();
+        });
 
 
-			storyheader = $('<div class="head"></div>');
-			story.append(storyheader);
+        request.fail(function(jqXHR, textStatus) {
+            fimfic.isOnline = false;
+            fimfic.isLoggedIn = false;
+            fimfic.listStoriesStatus = 'success';
+            callback.call();
+        });
+    },
 
-			// <div class="statback"><div class="statbulb ready"></div></div>
-			storyheader.append($('<div class="statback"><div class="statbulb notready"></div></div>'));
+    showListedStories: function () {
+        $('#content').append($('<div id="stories"></div>'));
 
-			storyheader.append($('<h2></h2>').text(this.title));
-			storyheader.append($('<span>&nbsp;&nbsp;by </span>'));
-			storyheader.append($('<span class="author"></span>').text(this.author));
+        $.each(fimfic.listedStories, function() {
+
+            story = $('<div class="story"></div>');
+            story.attr('fim_id', this.id); // because we mess with the fimfic.listedStories array,
+                                           //  we need this to be able to find correct divs later
+
+            storyheader = $('<div class="head"></div>');
+            story.append(storyheader);
+
+            storyheader.append($('<div class="statback"><div class="statbulb notready"></div></div>'));
+
+            storyheader.append($('<h2></h2>').text(this.title));
+            storyheader.append($('<span>&nbsp;&nbsp;by </span>'));
+            storyheader.append($('<span class="author"></span>').text(this.author));
 
 
-			storybody = $('<div class="body"></div>');
-			story.append(storybody);
+            storybody = $('<div class="body"></div>');
+            story.append(storybody);
 
-			storybody.append($('<span class="description"></span>').text(this.description));
+            storybody.append($('<span class="description"></span>').text(this.description));
 
-			$('#stories').append(story);
-		});
-	},
+            $('#stories').append(story);
+        });
+    },
 
-	// set isLoggedIn variable, given html
-	//
-	// we do this every request, as isLoggedIn is
-	//  only used as an info variable, rather than
-	//  to actually keep us from programatically
-	//  making requests... Nicer to keep everything
-	//  totally dynamic, I think.
-	checkLoggedIn: function (parsedhtml) {
-		// login_area div only shown
-		//  when user has yet to login
-		if ($(parsedhtml).find('#login_area').length > 0) {
-			fimfic.isLoggedIn = false;
-		} else {
-			fimfic.isLoggedIn = true;
-		}
-	}
+    // set isLoggedIn variable, given html
+    //
+    // we do this every request, as isLoggedIn is
+    //  only used as an info variable, rather than
+    //  to actually keep us from programatically
+    //  making requests... Nicer to keep everything
+    //  totally dynamic, I think.
+    checkLoggedIn: function (parsedhtml) {
+        // login_area div only shown
+        //  when user has yet to login
+        if ($(parsedhtml).find('#login_area').length > 0) {
+            fimfic.isLoggedIn = false;
+        } else {
+            fimfic.isLoggedIn = true;
+        }
+    }
 }
 
 
 $(document).ready(function () {
-	// Initialise database, if not done so already
-	fimfic.initDatabase();
+    // Initialise database, if not done so already
+    fimfic.initDatabase();
 
-	fimfic.updateStories(function (code) {
-		fimfic.showListedStories();
+    fimfic.updateStories(function (code) {
+        fimfic.showListedStories();
 
-		// Check 'net connection
-		if (fimfic.isLoggedIn) {
-			if (fimfic.listStoriesStatus == 'success') {
-				$('#status span').fadeOut(400, function () {
-					$('#status span').text("Read Later list obtained").fadeIn();
-				});
+        // Check DB up here somewhere, for if no network access
+        //  assume we fall through to below if we have network access,
+        //  or this is a new database with no info in it
 
-				$('#status').delay(4000).fadeOut(400);
-			} else {
-				$('#status span').fadeOut(400, function () {
-					$('#status span').text("Failed to access Read Later list").fadeIn();
-				});
-			}
-		} else if (fimfic.isOnline) {
-			$('#status span').fadeOut(400, function () {
-				$('#status span').text("You need to login to fimfiction to use this site").fadeIn();
-			});
-		} else {
-			$('#status span').fadeOut(400, function () {
-				$('#status span').text("You need internet access to download stories (or cross-origin ajax error)").fadeIn();
-			});
-		}
+        // Check 'net connection
+        if (fimfic.isLoggedIn) {
+            if (fimfic.listStoriesStatus == 'success') {
+                $('#status span').fadeOut(400, function () {
+                    $('#status span').text("Read Later list obtained").fadeIn();
+                });
 
-	});
+                $('#status').delay(4000).fadeOut(400);
+
+
+
+            } else {
+                $('#status span').fadeOut(400, function () {
+                    $('#status span').text("Failed to access Read Later list").fadeIn();
+                });
+            }
+        } else if (fimfic.isOnline) {
+            $('#status span').fadeOut(400, function () {
+                $('#status span').text("You need to login to fimfiction to use this site").fadeIn();
+            });
+        } else {
+            $('#status span').fadeOut(400, function () {
+                $('#status span').text("You need internet access to download stories (or cross-origin ajax error)").fadeIn();
+            });
+        }
+
+    });
 });
